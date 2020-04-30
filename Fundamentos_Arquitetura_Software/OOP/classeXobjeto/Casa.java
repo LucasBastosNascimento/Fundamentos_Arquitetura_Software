4b0d4bbbea835f708644eda995895416c47a3653
lucas bastos <lucas.bastos.nas@gmail.com>
2020-04-30 09:31:17 -0300

    classe vs objeto

---
+++ Fundamentos_Arquitetura_Software/OOP/classeXobjeto/Casa.java
@@ -0,0 +1,38 @@
+package classeXobjeto;
+
+public class Casa {
+	
+	public int TamanhoM2;
+	public int Andares;
+	public float Valor;
+	public int NumerosVagas;
+	
+	public int getTamanhoM2() {
+		return TamanhoM2;
+	}
+	public void setTamanhoM2(int tamanhoM2) {
+		TamanhoM2 = tamanhoM2;
+	}
+	public int getAndares() {
+		return Andares;
+	}
+	public void setAndares(int andares) {
+		Andares = andares;
+	}
+	public float getValor() {
+		return Valor;
+	}
+	public void setValor(float valor) {
+		Valor = valor;
+	}
+	public int getNumerosVagas() {
+		return NumerosVagas;
+	}
+	public void setNumerosVagas(int numerosVagas) {
+		NumerosVagas = numerosVagas;
+	}
+	
+	
+	 
+
+}
---
+++ Fundamentos_Arquitetura_Software/OOP/classeXobjeto/Objeto.java
@@ -0,0 +1,14 @@
+package classeXobjeto;
+
+public class Objeto {
+	
+	public Objeto() {
+	Casa casa = new Casa();
+		
+		casa.setTamanhoM2(100);
+		casa.setAndares(2);
+		casa.setValor(1000000);
+		casa.setNumerosVagas(3);
+	}
+
+}
